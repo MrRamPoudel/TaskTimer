5de5bb8c2b46ce8ef50d4293dcc1ea3a1c845221
Ram Poudel <rp164@uakron.edu>
2023-08-29 23:32:41 -0400

    Remove unused states

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,5 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-#include <QFinalState>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -8,11 +7,13 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     ui->lcdNumber->display("00:00:00");
     timer = new QTimer(this);
+
     //create states
     QState* s0 = new QState();
     QState* s1 = new QState();
     QState* s2 = new QState();
-    QFinalState * s3 = new QFinalState();
+
+    // Assign property and transition
     s1->assignProperty(ui->pushButton, "text", "Stop");
     s2->assignProperty(ui->pushButton, "text", "Start");
     s0->addTransition(ui->pushButton, &QPushButton::clicked,s1);
@@ -23,8 +24,6 @@ MainWindow::MainWindow(QWidget *parent)
     timeState.addState(s0);
     timeState.addState(s1);
     timeState.addState(s2);
-    timeState.addState(s3);
-
     //Set initial state
     timeState.setInitialState(s0);
 
